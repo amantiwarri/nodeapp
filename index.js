55ec810204a85aee447be644ab7175c0757ef012
Aman Tiwari <amantiwarri@gmail.com>
2024-12-31 17:04:53 +0530

    Update route error 31122024

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.staus(200).json({
-    message: "Welcome to the Home "});
+  res.status(200).json({
+    message: "Welcome to the Home ",
+  });
 });
 
 app.get("/users", (req, res) => {
-  res.staus(200).json({
-    message: "Welcome to the Users route"});
+  res.status(200).json({
+    message: "Welcome to the Users route",
+  });
 });
 
 app.get("/products", (req, res) => {
-  res.staus(200).json({
-    message: "Welcome to the Products route"});
+  res.status(200).json({
+    message: "Welcome to the Products route",
+  });
 });
 
 app.listen(port, () => {
