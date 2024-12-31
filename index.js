6d8b866f8e36575eb1041aa4dbf897565aa85356
Aman Tiwari <31343494+amantiwarri@users.noreply.github.com>
2024-12-31 14:56:44 +0530

    Add new users and products routes

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! from git repo and jenkins");
+  res.staus(200).json({
+    message: "Welcome to the Home "});
+});
+
+app.get("/users", (req, res) => {
+  res.staus(200).json({
+    message: "Welcome to the Users route"});
+});
+
+app.get("/products", (req, res) => {
+  res.staus(200).json({
+    message: "Welcome to the Products route"});
 });
 
 app.listen(port, () => {
